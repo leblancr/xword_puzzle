a1642139cf7a441b52b125abc4173027093121c6
leblancr <rich.leblanc1@proton.me>
2024-07-19 00:04:16 -0400

    set up models

--- tdd_exercise/xword_data/models.py
+++ tdd_exercise/xword_data/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Clue(models.Model):
+    entry = models.TextField()
+    puzzle = models.TextField()
+    clue_text = models.TextField()
+    theme = models.BooleanField(default=False)
+
+class Entry(models.Model):
+    entry_text = models.TextField(max_length=50)
+
+class Puzzle(models.Model):
+    title = models.TextField(max_length=255)
+    date = models.DateField()  # required (publication date)
+    byline = models.TextField(max_length=255)
+    publisher = models.TextField(max_length=12)
+
+
